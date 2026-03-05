754d570d6694b06b89ce762aa35357298a64e17e
rashedjamanraju <c251100@ugrad.iiuc.ac.bd>
2026-03-06 01:18:33 +0600

    codechef 800 happy new year

---
+++ 8.happy_new_year.cpp
@@ -0,0 +1,41 @@
+// https://www.codechef.com/problems/NEWYEAR
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl "\n"
+
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+using vll = vector<long long>;
+
+const int MOD = 1e9 + 7;
+const double PI = 3.141592653589793;
+
+// ---------- Fast IO ----------
+void fast_io() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+}
+
+void solve() {
+  // Write your solution here
+	int x;
+	cin>>x;
+	cout<<24-x<<endl;
+}
+
+int main() {
+  fast_io();
+
+  // int t;
+  // cin >> t;
+
+  // while (t--) {
+  //   solve();
+  // }
+
+  solve();
+
+  return 0;
+}
