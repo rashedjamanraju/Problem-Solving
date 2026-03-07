b5965f264e35120df1dc6f01987ca681e2f5944f
rashedjamanraju <c251100@ugrad.iiuc.ac.bd>
2026-03-07 21:14:56 +0600

    19

---
+++ 19.A_again_25.cpp
@@ -0,0 +1,41 @@
+// https://codeforces.com/problemset/problem/630/A
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl "\n"
+
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+using vll = vector<long long>;
+
+const int MOD = 1e9 + 7;
+const double PI = 3.141592653589793;
+
+// ---------- Fast IO ----------
+void fast_io() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+}
+
+void solve() {
+  // Write your solution here
+	int n;
+	cin>>n;
+	cout<<25<<endl;
+}
+
+int main() {
+  fast_io();
+
+  // int t;
+  // cin >> t;
+
+  // while (t--) {
+  //   solve();
+  // }
+
+  solve();
+
+  return 0;
+}
