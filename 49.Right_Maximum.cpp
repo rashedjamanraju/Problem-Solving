3a654dec5237c15a8f6893faf534e1926d040077
rashedjamanraju <c251100@ugrad.iiuc.ac.bd>
2026-03-17 04:32:45 +0600

    49

---
+++ 49.Right_Maximum.cpp
@@ -0,0 +1,101 @@
+// Problem Link: https://codeforces.com/contest/2204/problem/B
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl "\n"
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+using vll = vector<long long>;
+
+const int MOD = 1e9 + 7;
+const double PI = 3.141592653589793;
+
+// ---------- Fast IO ----------
+void fast_io() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+}
+
+// ------------ GCD ------------
+int __gcd(int a, int b) {
+  while (b != 0) {
+    int r = a % b;
+    a = b;
+    b = r;
+  }
+  return a;
+}
+
+// ---------- 4 Direction Neighbours ----------
+vector<pii> get4Neighbours(int x, int y, int n, int m) {
+  int dx[] = {-1, 1, 0, 0};
+  int dy[] = {0, 0, -1, 1};
+
+  vector<pii> nb;
+
+  for (int i = 0; i < 4; i++) {
+    int nx = x + dx[i];
+    int ny = y + dy[i];
+
+    if (nx >= 0 && nx < n && ny >= 0 && ny < m) nb.push_back({nx, ny});
+  }
+
+  return nb;
+}
+
+// ---------- 8 Direction Neighbours ----------
+vector<pii> get8Neighbours(int x, int y, int n, int m) {
+  int dx[] = {-1, -1, -1, 0, 0, 1, 1, 1};
+  int dy[] = {-1, 0, 1, -1, 1, -1, 0, 1};
+
+  vector<pii> nb;
+
+  for (int i = 0; i < 8; i++) {
+    int nx = x + dx[i];
+    int ny = y + dy[i];
+
+    if (nx >= 0 && nx < n && ny >= 0 && ny < m) nb.push_back({nx, ny});
+  }
+
+  return nb;
+}
+
+void solve() {
+  int n;
+  cin >> n;
+  vector<int> v(n, 0);
+  vector<int> max_placer(n, 0);
+  int mx = INT_MIN;
+  int count = 0;
+  for (int i = 0; i < n; ++i) {
+    cin >> v[i];
+    mx = max(mx, v[i]);
+    max_placer[i] = mx;
+    if (mx == v[i]) count++;
+  }
+  cout << count << endl;
+}
+
+int main() {
+  fast_io();
+
+  int t;
+  cin >> t;
+
+  while (t--) {
+    solve();
+  }
+
+  // solve()
+
+  return 0;
+}
